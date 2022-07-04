c070a591a5004b0554aafb00cdbb890ff2201e08
mherzet <65231677+mherzet@users.noreply.github.com>
2022-07-04 19:31:25 +0200

    Add files via upload

---
+++ diskformat.ps1
@@ -0,0 +1,5 @@
+﻿Initialize-Disk -Number 2
+New-Partition -DiskNumber 2 -UseMaximumSize -DriveLetter D
+Format-Volume -DriveLetter D -FileSystem NTFS -NewFileSystemLabel Data01 -Confirm:$false
+
+exit 0
